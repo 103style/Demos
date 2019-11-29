d963b099546b563cf6b6412f5ea2c327b5f4f2a2
103style <wtimexiaoke@gmail.com>
2019-11-29 11:44:46 +0800

    init annotationdemo

---
+++ AnnotationDemo/app/src/main/java/com/lxk/annotationdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.lxk.annotationdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author https://github.com/103style
+ * @date 2019/11/29 11:43
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
