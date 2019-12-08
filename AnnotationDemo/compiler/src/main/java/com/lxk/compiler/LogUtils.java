fb2db5d89ee836f5622fec1677d4ecf26d2523ef
103style <wtimexiaoke@gmail.com>
2019-12-08 18:02:42 +0800

    ipc demo init

---
+++ AnnotationDemo/compiler/src/main/java/com/lxk/compiler/LogUtils.java
@@ -0,0 +1,7 @@
+package com.lxk.compiler;
+
+/**
+ * created by 103style  2019/12/7 16:28
+ */
+public class LogUtils {
+}
---
+++ IPCDemo/app/src/main/java/com/lxk/ipcdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.lxk.ipcdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author https://github.com/103style
+ * @date 2019/12/8 18:02
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
