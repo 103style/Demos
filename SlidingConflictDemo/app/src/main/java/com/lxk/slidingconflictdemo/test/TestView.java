6358581f891443bc9d634155bd9f020b98cbb296
103style <wtimexiaoke@gmail.com>
2019-12-15 23:45:56 +0800

    update

--- SlidingConflictDemo/app/src/main/java/com/lxk/slidingconflictdemo/test/TestView.java
+++ SlidingConflictDemo/app/src/main/java/com/lxk/slidingconflictdemo/test/TestView.java
@@ -3,58 +3,50 @@ package com.lxk.slidingconflictdemo.test;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.ViewGroup;
+import android.view.View;
 
 /**
  * @author https://github.com/103style
- * @date 2019/12/15 17:11
+ * @date 2019/12/15 23:45
  */
-public class TestViewGroup extends ViewGroup {
+public class TestView extends View {
 
     private float lastEventX, lastEventY;
 
-    public TestViewGroup(Context context) {
+    public TestView(Context context) {
         super(context);
     }
 
-    public TestViewGroup(Context context, AttributeSet attrs) {
+    public TestView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public TestViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
+    public TestView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-    }
-
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        boolean intercept = false;
+    public boolean dispatchTouchEvent(MotionEvent ev) {
         float x = ev.getX();
         float y = ev.getY();
         switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                intercept = false;
+                getParent().requestDisallowInterceptTouchEvent(true);
                 break;
             case MotionEvent.ACTION_MOVE:
-
-//                if (父容器需要当前点击事件) {
-//                    intercept = true;
-//                } else {
-//                    intercept = false;
+                float dx = x - lastEventX;
+                float dy = y - lastEventY;
+//                if(父容器需要处理){
+//                    getParent().requestDisallowInterceptTouchEvent(false);
 //                }
                 break;
             case MotionEvent.ACTION_UP:
-                intercept = false;
                 break;
             default:
-                intercept = false;
                 break;
         }
         lastEventX = x;
         lastEventY = y;
-        return intercept;
+        return super.dispatchTouchEvent(ev);
     }
 }
