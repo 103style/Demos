84262f2674dad53d7c049734436869b41b11d3d3
103style <wtimexiaoke@gmail.com>
2020-01-09 10:21:10 +0800

    init

---
+++ SurfaceViewDemo/app/src/main/java/com/lxk/surfaceviewdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.lxk.surfaceviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author https://github.com/103style
+ * @date 2020/1/9 10:19
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
