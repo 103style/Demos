e1c8e38d759f17bca561cee5f341370a123e2aa2
103style <wtimexiaoke@gmail.com>
2020-01-13 21:20:31 +0800

    init windowdemo"

---
+++ WindowDemo/app/src/main/java/com/lxk/windowdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.lxk.windowdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author https://github.com/103style
+ * @date 2020/1/13 21:18
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
