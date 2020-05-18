fca7c3b72d3776b101d570a6cfc0fc44dad8bfb0
103style <wtimexiaoke@gmail.com>
2020-05-18 20:26:53 +0800

    bug

--- PluginAppDemo/plugin/src/main/java/com/lxk/plugin/PluginActivity.java
+++ PluginAppDemo/plugin/src/main/java/com/lxk/plugin/PluginActivity.java
@@ -1,5 +1,7 @@
 package com.lxk.plugin;
 
+import android.content.res.AssetManager;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,16 @@ import androidx.appcompat.app.AppCompatActivity;
  * @date 2020/5/5 21:52
  */
 public class PluginActivity extends AppCompatActivity {
+
+    @Override
+    public Resources getResources() {
+        return getApplication() != null && getApplication().getResources() != null ? getApplication().getResources() : super.getResources();
+    }
+
+    @Override
+    public AssetManager getAssets() {
+        return getApplication() != null && getApplication().getAssets() != null ? getApplication().getAssets() : super.getAssets();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
